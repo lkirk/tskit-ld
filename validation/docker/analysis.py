d7043eca184e873629b89efcfba95b2fef54c946
lkirk <kirklloyd@gmail.com>
2024-05-20 12:56:16 -0500

    computations were incorrect, get diagonals for distance

--- validation/docker/analysis.py
+++ validation/docker/analysis.py
@@ -9,54 +9,27 @@ import numpy as np
 TSKIT_PROTO_DIR = "/tskit/python"
 if TSKIT_PROTO_DIR not in sys.path:
     sys.path.insert(0, TSKIT_PROTO_DIR)
-from tests.test_ld_matrix import ld_matrix, positions_to_tree_indices, get_index_repeats
+from tests.test_ld_matrix import ld_matrix
 
 
-def get_upper_tri_no_diag(arr):
-    idx = np.zeros_like(arr, dtype=bool)
-    idx[np.triu_indices_from(arr)] = True
-    idx &= ~np.eye(len(arr), dtype=bool)
-    return idx
-
-
-def upper_off_diag_mean(arr):
-    return arr[get_upper_tri_no_diag(arr)].mean()
-
-
-def position_array(ts, stat_arr, pos):
-    idx = positions_to_tree_indices(ts.breakpoints(as_array=True), pos)
-    result = np.zeros((len(idx), len(idx)))
-    repeats = get_index_repeats(idx)
-    row = 0
-    for r in range(ts.num_trees):
-        col = 0
-        for c in range(ts.num_trees):
-            for i in range(repeats[r]):
-                for j in range(repeats[c]):
-                    result[i + row, j + col] = stat_arr[r, c]
-            col += repeats[c]
-        row += repeats[r]
-    return result
-
-
-def get_mean_stat_for_all_positions(ts, stat_mat, positions):
+def mean_stat_at_distances(stat, num_positions):
     return np.array(
         [
-            upper_off_diag_mean(position_array(ts, stat_mat, positions[k]))
-            for k in positions
+            stat[np.eye(num_positions, k=i, dtype=bool)].mean()
+            for i in range(num_positions)
         ]
     )
 
 
 def compute_mean_stat_for_recomb_distances(ts, positions, mu):
-    d2 = ld_matrix(ts, stat="d2_unbiased", mode="branch")
-    pi2 = ld_matrix(ts, stat="pi2_unbiased", mode="branch")
-    dz = ld_matrix(ts, stat="dz_unbiased", mode="branch")
+    d2 = ld_matrix(ts, stat="d2_unbiased", mode="branch", positions=[positions])
+    pi2 = ld_matrix(ts, stat="pi2_unbiased", mode="branch", positions=[positions])
+    dz = ld_matrix(ts, stat="dz_unbiased", mode="branch", positions=[positions])
     return np.vstack(
         [
-            get_mean_stat_for_all_positions(ts, d2, positions),
-            get_mean_stat_for_all_positions(ts, dz, positions),
-            get_mean_stat_for_all_positions(ts, pi2, positions),
+            mean_stat_at_distances(d2, len(positions)),
+            mean_stat_at_distances(dz, len(positions)),
+            mean_stat_at_distances(pi2, len(positions)),
         ]
     ) * (mu**2)
 
@@ -75,12 +48,13 @@ def main():
     r = 1.25e-5
     sequence_length = 11
 
-    logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s', level=logging.INFO)
+    logging.basicConfig(
+        format="%(asctime)s %(levelname)s %(message)s", level=logging.INFO
+    )
     logger = logging.getLogger(__name__)
 
-    positions = dict()
-    for i in range(1, sequence_length - 1):
-        positions[i] = np.arange(0, sequence_length, r * i * Ne)
+    # Each basepair position in the tree sequence
+    positions = np.arange(0, sequence_length)
 
     logger.info("Processing rep=%s seeds=%s cwd=%s", args.rep, args.seeds, os.getcwd())
     results = []
