8f3596fca48890799d4d5a94098f235b77cc0bfc
lkirk <kirklloyd@gmail.com>
2024-07-14 14:50:28 -0500

    it would help to commit the module

---
+++ src/simulations/slim.py
@@ -0,0 +1,51 @@
+import subprocess
+from pathlib import Path
+
+import structlog
+from pydantic import BaseModel, field_validator
+
+
+class SlimParams(BaseModel):
+    K: int
+    SD: float
+    SI: float
+    SM: float
+    L: int
+    W: float
+    G: float
+    MU: int
+    R: float
+    SIM_END: float
+    IND_RECORD_LIM: int
+    IND_RECORD_LAG: int
+    IND_RECORD_FREQ: float
+    OUTPATH: Path
+
+    @field_validator("OUTPATH")
+    @classmethod
+    def contains_units(cls, v: Path) -> Path:
+        assert not v.exists(), f"{v} already exists"
+        return v
+
+
+class SLiMJobArgs(BaseModel):
+    in_files: None
+    out_files: Path
+    params: SlimParams
+
+
+def main(args: SLiMJobArgs) -> None:
+    log = structlog.get_logger()
+    log.info("starting", workflow="test")
+
+    params_file = Path("params.json")
+    if params_file.exists():
+        raise Exception(f"{params_file} already exists")
+
+    with open(params_file, "w") as fp:
+        fp.write(args.params.model_dump_json())
+
+    log.info("running SLiM")
+    subprocess.run(
+        ["slim", "-d", "PARAM_FILE='params.json'", "/opt/main.slim"], check=True
+    )
