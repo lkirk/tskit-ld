3c4df0ce808128ecba758d55fdcf757d76e81e1e
lkirk <kirklloyd@gmail.com>
2024-08-05 22:58:48 -0500

    add a float64array type

--- src/tskit_ld/types.py
+++ src/tskit_ld/types.py
@@ -8,6 +8,7 @@ NPShapeLike = Optional[Sequence | SupportsIndex]  # (as defined in the _typing m
 NPInt64Array = npt.NDArray[np.int64]
 NPUInt32Array = npt.NDArray[np.uint32]
 NPInt32Array = npt.NDArray[np.int32]
+NPFloat64Array = npt.NDArray[np.float64]
 
 # Type alias for polars expressions
 IntoExpr = polars.type_aliases.IntoExpr
