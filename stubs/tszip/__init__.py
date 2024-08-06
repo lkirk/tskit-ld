7c634ad897f043d055995b9cf0efcb8e210d77a7
lkirk <kirklloyd@gmail.com>
2024-08-05 22:59:32 -0500

    remove superfluous stub file

--- stubs/tszip/__init__.py
+++
@@ -1,9 +0,0 @@
-from pathlib import Path
-
-import tskit
-
-
-def load(path: Path | int) -> tskit.TreeSequence: ...
-def compress(
-    ts: tskit.TreeSequence, destination: Path, variants_only: bool = False
-) -> None: ...
